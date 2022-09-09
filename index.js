3d658a2b5b2aa67d7f6286c03d3288afb0cf1cdc
Rahad Khan <clash146098@gmail.com>
2022-09-09 19:56:39 +0600

    modified again with response

--- index.js
+++ index.js
@@ -8,27 +8,13 @@ const userRouter = require('./routes/users.route');
 
 app.use(cors())
 app.use(express.json());
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.send("Server Running");
+    res.send("Welcome to User server");
 });
 
-app.use('/user', userRouter)
+app.use('/user', userRouter);
 
-// app.get('/user/all', (req, res) => {
-//     fs.readFile("users.json", (err, data) => {
-//         if (err) {
-//             res.write("Something Wrong !!!");
-//             res.end()
-//         } else {
-//             const users = JSON.parse(data);
-//             res.send(users)
-//         }
-//     })
-// })
-
-console.log(app)
 app.listen(port, () => {
     console.log("Port is running at ", port);
 })
